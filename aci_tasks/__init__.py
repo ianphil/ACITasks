f9a77a2efb831843f9543e14c8ed841e4b9a65f1
Ian Philpot <ian.philpot@microsoft.com>
2019-11-25 13:19:49 -0500

    Updates tests to include container group

---
+++ aci_tasks/__init__.py
@@ -0,0 +1,11 @@
+# from cloud import (
+#     ContainerInstance,
+#     ResourceGroup,
+#     ContainerGroupInstance
+# )
+
+# __all__ = [
+#     'ContainerInstance',
+#     'ResourceGroup',
+#     'ContainerGroupInstance'
+# ]
---
+++ test/test_container_group.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import time
+from pytest import fixture
+from aci_tasks.cloud import (
+    ResourceGroup,
+    ContainerGroupInstance,
+    ContainerInstance
+)
+from aci_tasks.logger import Logger
+from aci_tasks.config import Config, TaskType
+
+
+@fixture(scope="session", autouse=True)
+def setup():
+    rg = ResourceGroup(Logger(), Config(TaskType.STRONG))
+    rg.create()
+    yield
+    rg.delete()
+
+
+@fixture
+def strong_container_instance():
+    return ContainerInstance(Logger(), Config(TaskType.STRONG))
+
+
+@fixture
+def strong_container_group():
+    return ContainerGroupInstance(Logger(), Config(TaskType.STRONG))
+
+
+@fixture
+def weak_container_group():
+    return ContainerGroupInstance(Logger(), Config(TaskType.WEAK))
+
+
+def test_strong_create(strong_container_group):
+    strong_container_group.create()
+    assert strong_container_group._exists() is True
+
+
+def test_strong_execute(strong_container_instance):
+    '''This will post body=dog to https://hookbin.com/mZz671L0bXCBVLpJXLkx'''
+    strong_container_instance.execute_command('/strong_task/dog.py')
+    assert 1 == 1  # TODO: need to figureout how to assert this
+
+
+def test_strong_delete(strong_container_group):
+    strong_container_group.delete()
+    assert strong_container_group._exists() is False
+
+
+def test_weak_create(weak_container_group):
+    '''This will post body=cat to https://hookbin.com/mZz671L0bXCBVLpJXLkx'''
+    weak_container_group.create()
+    assert weak_container_group._exists() is True
+
+
+def test_weak_stop(weak_container_group):
+    weak_container_group.stop()
+    time.sleep(60)
+    assert 1 == 1  # TODO: need to figureout how to assert this
+
+
+def test_weak_start(weak_container_group):
+    '''This will post body=cat to https://hookbin.com/mZz671L0bXCBVLpJXLkx'''
+    weak_container_group.start()
+    assert 1 == 1  # TODO: need to figureout how to assert this
+
+
+def test_weak_delete(weak_container_group):
+    weak_container_group.delete()
+    assert weak_container_group._exists() is False
+
+
+# To verify tests there should be a dog and two cats posted to:
+# https://hookbin.com/mZz671L0bXCBVLpJXLkx
